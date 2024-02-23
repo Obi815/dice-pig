79e797d6a40ee2dd7eb48cc059a9b7688995e450
Obi815 <80441506+Obi815@users.noreply.github.com>
2024-02-22 20:28:37 -0800

    Finished the game

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dice-pig
 Play the game pig.
+
+HOW TO PLAY. -->
+When it is your turn you will role a dice.
+When roll you add up the numbers the dice lands on
+You will play to a target score. 
+You keep rolling the dice until you roll a 1
+Once you roll a 1 your turn is over and you go back to zero. 
--- game.py
+++ game.py
@@ -20,10 +20,41 @@ while True:
             print("Try Again...")
     else:
         print("Must be a valid number between (2-4)")
-print(players)
+
 
 max_score = 20
 players_scores = [0 for _ in range(players)]
 
-print(players_scores)
+while max(players_scores) < max_score:
+    for player_idx in range(players):
+        print("\nPlayer", player_idx + 1, "turn.\n")
+        print("Your score is:", players_scores[player_idx])
+        current_score = 0 
+
+        while True:
+            should_roll = input("Would you like to roll (y)? ")
+            if should_roll.lower() != 'y':
+                break
+            
+            value = roll()
+            if value == 1:
+                print("You rolled a 1... TURN OVER")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("Your rolled a: ", value)
+
+            print("Your score is: ", current_score)
+
+        players_scores[player_idx] += current_score
+        print("Your total score is:", players_scores[player_idx])
+
+max_score = max(players_scores)
+winning_idx = players_scores.index(max_score)
+print("Player number", winning_idx + 1,
+      "is the winner with the score of:", max_score)
+
+
+
 
